3e1a2de61c1e8c62a168699ef7f82cb3fba65d2d
Radu David <alexradu07@yahoo.com>
2021-01-06 17:02:36 +0200

    Added camera following player

---
+++ Assets/Scripts/Running Game Scripts/MapLoaderRunningGame.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Maps.Coord;
+using Google.Maps.Event;
+using Google.Maps;
+
+//namespace Diploma
+//{
+
+[RequireComponent(typeof(MapsService))]
+public class MapLoaderRunningGame : MonoBehaviour
+{
+    private LatLng latLng = new LatLng(0, 0);
+
+    //public MapsService MapsService;
+    public MapsService mapsService;
+    public GameObjectOptions DefaultGameObjectOptions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void AddCollidersToBuildings(MapLoadedArgs args)
+    {
+        GameObject[] buildingObjects = GameObject.FindObjectsOfType<GameObject>();
+        // Debug.Log(buildingObjects.Length);
+        foreach (GameObject obj in buildingObjects)
+        {
+            // if (obj.name.StartsWith("ExtrudedStructure "))
+            if (obj.transform.parent != null && obj.transform.parent.name == "GoogleMaps")
+            {
+                obj.AddComponent<MeshCollider>();
+            }
+        }
+    }
+
+    public void LoadMap(double lat, double lng)
+    {
+        latLng = new LatLng(lat, lng);
+
+        Debug.Log("Start start");
+        // Get required MapsService component on this GameObject.
+        mapsService = GetComponent<MapsService>();
+
+        // Set real-world location to load.
+        mapsService.InitFloatingOrigin(latLng);
+
+        // Register a listener to be notified when the map is loaded.
+        // mapsService.Events.MapEvents.Loaded.AddListener(OnLoaded);
+
+        // Load map with default options.
+        DefaultGameObjectOptions = DefaultStyles.getDefaultStyles();
+        mapsService.LoadMap(new Bounds(Vector3.zero, new Vector3(500, 0, 500)), DefaultGameObjectOptions);
+
+        mapsService.Events.MapEvents.Loaded.AddListener(AddCollidersToBuildings);
+        // mapsService.Events.MapEvents.Loaded.AddListener(RoadScript.GenerateRoad);
+        mapsService.Events.MapEvents.Loaded.AddListener(GameObject.Find("RoadController").GetComponent<RoadScript>().GenerateRoad);
+
+        Debug.Log("Start end");
+    }
+}
+
+//}
---
+++ Assets/Scripts/Running Game Scripts/RoadScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Maps.Unity.Intersections;
+using Google.Maps;
+using Google.Maps.Event;
+
+public class RoadScript : MonoBehaviour
+{
+    private RoadLatticeNode startNode;
+    private RoadLatticeNode endNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MapsService mapsService = GameObject.Find("GoogleMaps").GetComponent<MapLoaderRunningGame>().mapsService;
+        GameObject player = GameObject.Find("Player");
+        Camera mainCamera = Camera.main;
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            startNode = endNode;
+            player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y);
+
+            List<RoadLatticeNode> neighbors = new List<RoadLatticeNode>(startNode.Neighbors);
+            endNode = neighbors[neighbors.Count - 1];
+
+            float slope = (startNode.Location.y - endNode.Location.y) / (startNode.Location.x - endNode.Location.x);
+            Debug.Log(slope);
+            Debug.Log(startNode.Location.x);
+            Debug.Log(startNode.Location.y);
+            Debug.Log(endNode.Location.x);
+            Debug.Log(endNode.Location.y);
+
+            mainCamera.transform.position = new Vector3(player.transform.position.x - 5 * Mathf.Cos(Mathf.Atan(slope)),
+                                                        mainCamera.transform.position.y,
+                                                        player.transform.position.z - 5 * Mathf.Sin(Mathf.Atan(slope)));
+            mainCamera.transform.eulerAngles = new Vector3(mainCamera.transform.eulerAngles.x,
+                                                        90 - Mathf.Atan(slope) * 180 / Mathf.PI,
+                                                        mainCamera.transform.eulerAngles.z);
+        }
+    }
+
+    public void GenerateRoad(MapLoadedArgs args)
+    {
+        MapsService mapsService = GameObject.Find("GoogleMaps").GetComponent<MapLoaderRunningGame>().mapsService;
+        GameObject player = GameObject.Find("Player");
+        Camera mainCamera = Camera.main;
+
+        List<RoadLatticeNode> nodes = new List<RoadLatticeNode>(mapsService.RoadLattice.Nodes);
+
+        float minX = float.MaxValue;
+        ulong startPosUID = 0;
+        foreach (RoadLatticeNode node in nodes)
+        {
+            if (node.Location.x < minX)
+            {
+                minX = node.Location.x;
+                startPosUID = node.LocationUID;
+            }
+        }
+
+        startNode = mapsService.RoadLattice.FindNodeAt(startPosUID);
+        player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y);
+
+        List<RoadLatticeNode> neighbors = new List<RoadLatticeNode>(startNode.Neighbors);
+        endNode = neighbors[0];
+
+        float slope = (startNode.Location.y - endNode.Location.y) / (startNode.Location.x - endNode.Location.x);
+        Debug.Log(slope);
+        Debug.Log(startNode.Location.x);
+        Debug.Log(startNode.Location.y);
+        Debug.Log(endNode.Location.x);
+        Debug.Log(endNode.Location.y);
+
+        mainCamera.transform.position = new Vector3(player.transform.position.x - 5 * Mathf.Cos(Mathf.Atan(slope)),
+                                                    mainCamera.transform.position.y,
+                                                    player.transform.position.z - 5 * Mathf.Sin(Mathf.Atan(slope)));
+        mainCamera.transform.eulerAngles = new Vector3(mainCamera.transform.eulerAngles.x,
+                                                    90 - Mathf.Atan(slope) * 180 / Mathf.PI,
+                                                    mainCamera.transform.eulerAngles.z);
+
+    }
+}
