4c62682c7d251e3ba042c515891a9ed3d102aed2
Radu David <alexradu07@yahoo.com>
2021-01-08 19:53:41 +0200

    Added lanes switching on the road

--- Assets/Scripts/Running Game Scripts/RoadScript.cs
+++ Assets/Scripts/Running Game Scripts/RoadScript.cs
@@ -8,12 +8,20 @@ using Google.Maps.Event;
 public class RoadScript : MonoBehaviour
 {
     public float movementSpeed;
+    public float lateralMovementAmount;
 
     private RoadLatticeNode startNode;
     private RoadLatticeNode endNode;
     private float slope;
 
     private List<RoadLatticeNode> visitedNodes = new List<RoadLatticeNode>();
+
+    /*
+     * -1 -> left
+     *  0 -> center
+     *  1 -> right
+     */
+    private int roadPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +30,36 @@ public class RoadScript : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Move to the left
+            // player.GetComponent<Rigidbody>().velocity = new Vector3(endNode.Location.x - startNode.Location.x, 0, startNode.Location.y - endNode.Location.y).normalized * movementSpeed;
+            // player.transform.position = player.transform.position + new Vector3(endNode.Location.x - startNode.Location.x, 0, startNode.Location.y - endNode.Location.y).normalized;
+            player.transform.position = player.transform.position + Vector3.Cross(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * lateralMovementAmount,
+                                                    Vector3.up);
+            if (roadPosition > -1)
+            {
+                roadPosition--;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Move to the right
+            // player.GetComponent<Rigidbody>().velocity = new Vector3(startNode.Location.x - endNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * movementSpeed;
+            // player.transform.position = player.transform.position + new Vector3(startNode.Location.x - endNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized;
+            player.transform.position = player.transform.position - Vector3.Cross(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * lateralMovementAmount,
+                                                    Vector3.up);
+            if (roadPosition < 1)
+            {
+                roadPosition++;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         if (startNode == null)
@@ -34,9 +72,9 @@ public class RoadScript : MonoBehaviour
         Camera mainCamera = Camera.main;
 
         // player.GetComponent<Rigidbody>().AddForce(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y), ForceMode.Force);
+
         player.GetComponent<Rigidbody>().velocity = new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * movementSpeed;
 
-        
         if ((endNode.Location.x < startNode.Location.x && endNode.Location.y > startNode.Location.y)
             || (endNode.Location.x < startNode.Location.x && endNode.Location.y < startNode.Location.y))
         {
@@ -45,50 +83,86 @@ public class RoadScript : MonoBehaviour
             mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(player.transform.position.x + 5 * Mathf.Cos(Mathf.Atan(slope)),
                                                             mainCamera.transform.position.y,
                                                             player.transform.position.z + 5 * Mathf.Sin(Mathf.Atan(slope))), 0.1f);
+            /*
             mainCamera.transform.eulerAngles = Vector3.Lerp(new Vector3(mainCamera.transform.eulerAngles.x,
                                                         -90-45 - Mathf.Atan(slope) * 180 / Mathf.PI,
                                                         mainCamera.transform.eulerAngles.z), mainCamera.transform.eulerAngles, 0.1f);
+                                                        */
+            mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, Quaternion.Euler(new Vector3(mainCamera.transform.eulerAngles.x,
+                                                        -90 - Mathf.Atan(slope) * 180 / Mathf.PI,
+                                                        mainCamera.transform.eulerAngles.z)), 0.1f);
         } else
         {
             mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(player.transform.position.x - 5 * Mathf.Cos(Mathf.Atan(slope)),
                                                         mainCamera.transform.position.y,
                                                         player.transform.position.z - 5 * Mathf.Sin(Mathf.Atan(slope))), 0.1f);
+            /*
             mainCamera.transform.eulerAngles = Vector3.Lerp(mainCamera.transform.eulerAngles, new Vector3(mainCamera.transform.eulerAngles.x,
                                                         90 - Mathf.Atan(slope) * 180 / Mathf.PI,
                                                         mainCamera.transform.eulerAngles.z), 0.1f);
+                                                        */
+            mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, Quaternion.Euler(new Vector3(mainCamera.transform.eulerAngles.x,
+                                                        90 - Mathf.Atan(slope) * 180 / Mathf.PI,
+                                                        mainCamera.transform.eulerAngles.z)), 0.1f);
         }
 
+        // Calculate player center position
+        Vector3 playerCenterPosition = player.transform.position;
+        if (roadPosition == -1)
+        {
+            playerCenterPosition = player.transform.position
+                    - Vector3.Cross(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * lateralMovementAmount, Vector3.up);
+        } else if (roadPosition == 1) {
+            playerCenterPosition = player.transform.position
+                    + Vector3.Cross(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * lateralMovementAmount, Vector3.up);
+        }
 
-        // if (Input.GetKeyDown(KeyCode.N))
+        // Player passed the endNode, new segment is used
         if ((endNode.Location.x > startNode.Location.x && endNode.Location.y > startNode.Location.y
-            && player.transform.position.x > endNode.Location.x && player.transform.position.z > endNode.Location.y)
+            && playerCenterPosition.x >= endNode.Location.x && playerCenterPosition.z >= endNode.Location.y)
             || (endNode.Location.x < startNode.Location.x && endNode.Location.y < startNode.Location.y
-            && player.transform.position.x < endNode.Location.x && player.transform.position.z < endNode.Location.y)
+            && playerCenterPosition.x <= endNode.Location.x && playerCenterPosition.z <= endNode.Location.y)
             || (endNode.Location.x > startNode.Location.x && endNode.Location.y < startNode.Location.y
-            && player.transform.position.x > endNode.Location.x && player.transform.position.z < endNode.Location.y)
+            && playerCenterPosition.x >= endNode.Location.x && playerCenterPosition.z <= endNode.Location.y)
             || (endNode.Location.x < startNode.Location.x && endNode.Location.y > startNode.Location.y
-            && player.transform.position.x < endNode.Location.x && player.transform.position.z > endNode.Location.y))
+            && playerCenterPosition.x <= endNode.Location.x && playerCenterPosition.z >= endNode.Location.y))
         {
             startNode = endNode;
-            player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y);
-
             visitedNodes.Add(startNode);
 
+            // Choose neighbor
             List<RoadLatticeNode> neighbors = new List<RoadLatticeNode>(startNode.Neighbors);
             endNode = neighbors[neighbors.Count - 1];
             if (visitedNodes.Contains(endNode))
             {
                 endNode = neighbors[neighbors.Count - 2];
             }
+            if (roadPosition == 0)
+            {
+                // Center position
+                player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y);
+            }
+            else if (roadPosition == 1)
+            {
+                // Right position
+                //player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y)
+                //    + new Vector3(endNode.Location.x - startNode.Location.x, 0, startNode.Location.y - endNode.Location.y).normalized;
+                player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y)
+                    - Vector3.Cross(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * lateralMovementAmount, Vector3.up);
+            }
+            else if (roadPosition == -1)
+            {
+                // Left position
+                //player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y)
+                //    + new Vector3(startNode.Location.x - endNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized;
+                player.transform.position = new Vector3(startNode.Location.x, player.transform.position.y, startNode.Location.y)
+                    + Vector3.Cross(new Vector3(endNode.Location.x - startNode.Location.x, 0, endNode.Location.y - startNode.Location.y).normalized * lateralMovementAmount, Vector3.up);
+            }
+            // player.transform.position = Vector3.Lerp(player.transform.position, new Vector3(startNode.Location.x, 0, startNode.Location.y), 0.1f);
+
 
             slope = (startNode.Location.y - endNode.Location.y) / (startNode.Location.x - endNode.Location.x);
             Debug.Log(slope);
-            /*
-            Debug.Log(startNode.Location.x);
-            Debug.Log(startNode.Location.y);
-            Debug.Log(endNode.Location.x);
-            Debug.Log(endNode.Location.y);
-            */
             
         }
     }
