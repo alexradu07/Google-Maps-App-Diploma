ed11cbf1376fae4c1fb43a49369f227359a5fc6e
mihaipapa <papa.mihaieugen@gmail.com>
2021-01-22 16:21:39 +0200

    carGame checkpoints

--- Assets/Scripts/Car Game Scripts/CarController.cs
+++ Assets/Scripts/Car Game Scripts/CarController.cs
@@ -16,6 +16,15 @@ public class CarController : MonoBehaviour
     public float cameraZ;
     public Camera cam;
     // Start is called before the first frame update
+    public void UpdateWheelPosition(WheelCollider col, Transform tran)
+    {
+        Vector3 position = tran.position;
+        Quaternion rotation = tran.rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        tran.position = position;
+        tran.rotation = rotation;
+    }
     void Start()
     {
       
@@ -45,22 +54,32 @@ public class CarController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.S))
         {
-            FR.brakeTorque = power;
-            FL.brakeTorque = power;
-            RR.brakeTorque = power;
-            RR.brakeTorque = power;
-        }
-        if (!Input.GetKey(KeyCode.W))
-        {
-            FR.motorTorque = 0;
-            FL.motorTorque = 0;
+            FR.brakeTorque = 1000;
+            FL.brakeTorque = 1000;
+            RR.brakeTorque = 1000;
+            RL.brakeTorque = 1000;
         }
         if (!Input.GetKey(KeyCode.S))
         {
             FR.brakeTorque = 0;
             FL.brakeTorque = 0;
             RR.brakeTorque = 0;
+            RL.brakeTorque = 0;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            RR.brakeTorque = 10000;
+            RL.brakeTorque = 10000;
+        }
+        if (!Input.GetKey(KeyCode.Space))
+        {
             RR.brakeTorque = 0;
+            RL.brakeTorque = 0;
+        }
+        if (!Input.GetKey(KeyCode.W))
+        {
+            FR.motorTorque = 0;
+            FL.motorTorque = 0;
         }
         if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
@@ -75,11 +94,15 @@ public class CarController : MonoBehaviour
         }
         FR.steerAngle = angle;
         FL.steerAngle = angle;
-        fr_tran.rotation = Quaternion.Euler(fr_tran.eulerAngles.x, car_tran.eulerAngles.y + angle, car_tran.eulerAngles.z);
-        fl_tran.rotation = Quaternion.Euler(fl_tran.eulerAngles.x, car_tran.eulerAngles.y + angle, car_tran.eulerAngles.z);
+        //fr_tran.rotation = Quaternion.Euler(fr_tran.eulerAngles.x, car_tran.eulerAngles.y + angle, car_tran.eulerAngles.z);
+        //fl_tran.rotation = Quaternion.Euler(fl_tran.eulerAngles.x, car_tran.eulerAngles.y + angle, car_tran.eulerAngles.z);
         //rr_tran.rotation = Quaternion.Euler(Mathf.Abs(car_rigid.velocity.z) + Mathf.Abs(car_rigid.velocity.x) + fr_tran.eulerAngles.x, car_tran.eulerAngles.y, 0);
         //rl_tran.rotation = Quaternion.Euler(Mathf.Abs(car_rigid.velocity.z) + Mathf.Abs(car_rigid.velocity.x) + fl_tran.eulerAngles.x, car_tran.eulerAngles.y, 0);
-        Debug.Log(car_rigid.velocity);
-        cam.transform.position = this.transform.position + new Vector3(cameraX, cameraY, cameraZ);
+        //Debug.Log(car_rigid.velocity);
+        //cam.transform.position = this.transform.position + new Vector3(cameraX, cameraY, cameraZ);
+        UpdateWheelPosition(FR, fr_tran);
+        UpdateWheelPosition(FL, fl_tran);
+        UpdateWheelPosition(RR, rr_tran);
+        UpdateWheelPosition(RL, rl_tran);
     }
 }
