b3948f14766d5be1b281de4bc99b8ef3fbabc4f8
Andrei Sosea <sosea.andrei@yahoo.com>
2021-03-31 22:33:19 +0300

    Fixed issue with synchronization and http request from current location

--- Assets/Scripts/Delivery Game Scripts/DeliverySceneController.cs
+++ Assets/Scripts/Delivery Game Scripts/DeliverySceneController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Android;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+using Debug = UnityEngine.Debug;
 
 public class DeliverySceneController : MonoBehaviour
 {
@@ -29,34 +33,38 @@ public class DeliverySceneController : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
+        //Debug.Log(GetCurrentMethod());
         SceneManager.LoadScene(sceneName);
     }
 
     public void SelectLocation()
     {
+        //Debug.Log(GetCurrentMethod());
         string locationString = GameObject.Find("Canvas/Panel/LocationDropdownSelector/Label").GetComponent<Text>().text;
         switch (locationString)
         {
             case "Grozavesti":
                 GameObject.Find("GoogleMaps").GetComponent<DeliveryMapLoader>().LoadMap(44.4433837, 26.0618934);
+                Manager.gameStarted = true;
                 break;
             case "Unirii":
                 GameObject.Find("GoogleMaps").GetComponent<DeliveryMapLoader>().LoadMap(44.426929, 26.1011807);
+                Manager.gameStarted = true;
                 break;
             case "Brasov":
                 GameObject.Find("GoogleMaps").GetComponent<DeliveryMapLoader>().LoadMap(45.6431122, 25.5858238);
+                Manager.gameStarted = true;
                 break;
-            //case "Current Location":
-            //    if (!Permission.HasUserAuthorizedPermission(Permission.CoarseLocation))
-            //    {
-            //        Permission.RequestUserPermission(Permission.CoarseLocation);
-            //    }
-            //    StartCoroutine(GetCurrentLocation());
-            //    break;
+            case "Current Location":
+                if (!Permission.HasUserAuthorizedPermission(Permission.CoarseLocation))
+                {
+                    Permission.RequestUserPermission(Permission.CoarseLocation);
+                }
+                StartCoroutine(GetCurrentLocation());
+                break;
             default:
                 break;
         }
-        Manager.gameStarted = true;
         GameObject.Find("Canvas/Panel").SetActive(false);
         minimap.SetActive(true);
         tukTukStatusDialog.SetActive(true);
@@ -64,6 +72,7 @@ public class DeliverySceneController : MonoBehaviour
 
     IEnumerator DisplayLocationError()
     {
+        //Debug.Log(GetCurrentMethod());
         locationError.SetActive(true);
         yield return new WaitForSeconds(1);
         locationError.SetActive(false);
@@ -71,6 +80,7 @@ public class DeliverySceneController : MonoBehaviour
 
     IEnumerator DisplayLoadingMessage()
     {
+        //Debug.Log(GetCurrentMethod());
         loadingMessage.SetActive(true);
         yield return new WaitForSeconds(2);
         loadingMessage.SetActive(false);
@@ -78,50 +88,54 @@ public class DeliverySceneController : MonoBehaviour
 
     IEnumerator GetCurrentLocation()
     {
+        //Debug.Log(GetCurrentMethod());
         StartCoroutine(DisplayLoadingMessage());
-        Debug.Log("1");
-        //yield return new WaitForSeconds(1);
-        Debug.Log("2");
+        yield return new WaitForSeconds(1);
         if (!Input.location.isEnabledByUser)
         {
-            Debug.Log("3");
             StartCoroutine(DisplayLocationError());
-            Debug.Log("4");
             yield break;
         }
-        Debug.Log("5");
         Input.location.Start();
-        Debug.Log("6");
-        //yield return new WaitForSeconds(1);
-        Debug.Log("7");
+        yield return new WaitForSeconds(1);
         while (Input.location.status == LocationServiceStatus.Initializing)
         {
             Debug.Log("Location is initializing");
         }
         if (Input.location.status == LocationServiceStatus.Failed)
         {
-            GameObject.Find("GoogleMaps").GetComponent<DeliveryMapLoader>().LoadMap(45, 25);
             Debug.Log("Location initialization failed");
             yield break;
         } else
         {
             Debug.Log("Latitude\t: " + Input.location.lastData.latitude);
             Debug.Log("Longitude\t: " + Input.location.lastData.longitude);
+            Manager.dynamicLatitude = Input.location.lastData.latitude;
+            Manager.dynamicLongitude = Input.location.lastData.longitude;
             GameObject.Find("GoogleMaps").GetComponent<DeliveryMapLoader>().LoadMap(Input.location.lastData.latitude, Input.location.lastData.longitude);
+            Manager.locationQueryComplete = true;
         }
-        Debug.Log("8");
 
-        //GameObject.Find("GoogleMaps").GetComponent<DeliveryMapLoader>().LoadMap(45, 25);
-        //yield return null;
         Input.location.Stop();
+        Manager.gameStarted = true;
     }
 
     public void onSelectVehicleButton()
     {
+        //Debug.Log(GetCurrentMethod());
         GameObject.Find("Canvas/Panel").SetActive(false);
         backButton.SetActive(true);
         Manager.choosingCar = true;
         leftButtonSelectVehicle.SetActive(true);
         rightButtonSelectVehicle.SetActive(true);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string GetCurrentMethod()
+    {
+        var st = new StackTrace();
+        var sf = st.GetFrame(1);
+
+        return sf.GetMethod().Name;
     }
 }
--- Assets/Scripts/Delivery Game Scripts/Manager.cs
+++ Assets/Scripts/Delivery Game Scripts/Manager.cs
@@ -1,6 +1,9 @@
 ﻿
 public static class Manager
 {
-    public static bool gameStarted = false;
-    public static bool choosingCar = false;
+    public static bool  gameStarted = false;
+    public static bool  choosingCar = false;
+    public static float dynamicLatitude = .0f;
+    public static float dynamicLongitude = .0f;
+    public static bool  locationQueryComplete = false;
 }
