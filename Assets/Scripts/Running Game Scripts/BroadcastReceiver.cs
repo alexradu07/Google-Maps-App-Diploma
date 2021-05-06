8b9a5eb22b22d788fcaeed2addf4a73f40afb335
Radu David <alexradu07@yahoo.com>
2021-05-06 18:41:10 +0300

    Running outdoor mode added

--- Assets/Scripts/Running Game Scripts/BroadcastReceiver.cs
+++ Assets/Scripts/Running Game Scripts/BroadcastReceiver.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class BroadcastReceiver : MonoBehaviour
 {
     public Text debugForPluginText;
+    public GameObject pin;
+
     private AndroidJavaClass jc;
     private string javaMessage = "";
+    private bool isMapInitialized;
+
+    private double lastLat;
+    private double lastLong;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,10 @@ public class BroadcastReceiver : MonoBehaviour
         // We call our java class function to create our MyReceiver java object
         jc.CallStatic("createInstance");
         debugForPluginText.text = "NULL AFTER START";
+
+        isMapInitialized = false;
+        lastLat = 0;
+        lastLong = 0;
 
         //startService("ro.pub.cs.systems.eim.unitylocationplugin.PluginStarter");
     }
@@ -42,6 +52,23 @@ public class BroadcastReceiver : MonoBehaviour
         if (latitude != 0 && longitude != 0)
         {
             debugForPluginText.text = latitude + " " + longitude;
+
+            if (!isMapInitialized)
+            {
+                isMapInitialized = true;
+                GameObject.Find("GoogleMaps").GetComponent<MapLoaderRunningGame>().LoadMap(latitude, longitude);
+                //GameObject.Find("Canvas/Panel").SetActive(false);
+            }
+
+            if (latitude != lastLat && longitude != lastLong)
+            {
+                Object.Instantiate(pin,
+                    GameObject.Find("GoogleMaps").GetComponent<MapLoaderRunningGame>().mapsService.Coords.FromLatLngToVector3(new Google.Maps.Coord.LatLng(latitude, longitude)),
+                    Quaternion.Euler(0, 0, 0));
+            }
+
+            lastLat = latitude;
+            lastLong = longitude;
         }
     }
 
